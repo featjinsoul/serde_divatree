ce2f6957a40d75b124b68b58bf67fbee6c40c314
waelwindows <waelwindows9922@gmail.com>
2022-12-20 04:49:23 +0300

    feat: Implement `Lexer` and `KeyValue`

--- src/de/mod.rs
+++ src/de/mod.rs
@@ -1,83 +1,97 @@
 use slab_tree::*;
 
-use std::ops::Deref;
+use std::iter::{Peekable, Take};
+use std::str::{Lines, Split};
 
 pub mod serde;
 pub use self::serde::from_str;
 
 use crate::error::ParseError;
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
-enum Node<'a> {
-    Key(&'a str),
-    Value(&'a str),
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Copy, Clone, Hash)]
+struct Lexer<I> {
+    lines: I,
 }
 
-struct A3daTree<'a> {
-    tree: Tree<Node<'a>>,
-    curr: NodeId,
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Copy, Clone, Hash)]
+struct KeyValue<'a> {
+    key: &'a str,
+    value: &'a str,
 }
 
-impl<'a> A3daTree<'a> {
-    fn new(input: &'a str) -> Result<A3daTree, ParseError> {
-        let lines = input.lines();
-        let mut tree = TreeBuilder::new().with_root(Node::Key("a3da_root")).build();
+impl<'a> KeyValue<'a> {
+    const PATH_DELIMITER: char = '.';
+    const KEY_VALUE_DELIMITER: char = '=';
 
-        for (line_num, line) in lines.enumerate() {
-            if line.is_empty() || line.starts_with('#') {
-                continue;
-            }
-            let mut splits = line.split('=');
-            let err = || ParseError {
-                line_num,
-                line: line.to_string(),
-            };
-            let lhs = splits.next().ok_or_else(err)?.trim();
-            let sections = lhs.split('.');
-            let rhs = splits.next().ok_or_else(err)?.trim();
-
-            //Guarranteed not to panic, since root is always in tree
-            let mut id = tree.root_id().unwrap();
-
-            for sec in sections {
-                let node = tree.get(id).unwrap();
-                match node.children().find(|x| x.data().deref() == sec) {
-                    Some(n) => {
-                        id = n.node_id();
-                        continue;
-                    }
-                    None => {
-                        let mut node = tree.get_mut(id).unwrap();
-                        let new = node.append(Node::Key(sec)).node_id();
-                        id = new;
-                    }
-                }
-            }
-            tree.get_mut(id).unwrap().append(Node::Value(rhs));
+    fn new(line: &'a str) -> Option<Self> {
+        let mut iter = line.split(Self::KEY_VALUE_DELIMITER);
+        let key = iter.next()?.trim();
+        if key.is_empty() {
+            return None;
         }
-        let curr = tree
-            .root()
-            .unwrap()
-            .first_child()
-            .unwrap_or(tree.root().unwrap())
-            .node_id();
-        Ok(Self { tree, curr })
+        let value = iter.next()?.trim();
+        if value.is_empty() {
+            return None;
+        }
+        Some(Self { key, value })
     }
-    fn print(&self) {
-        let mut str = String::new();
-        self.tree.write_formatted(&mut str).unwrap();
-        println!("{}", str);
+    fn path(&self) -> Split<'a, char> {
+        self.key.split(Self::PATH_DELIMITER)
+    }
+    fn strip(&self, levels: usize) -> Option<Self> {
+        let dot_pos = self
+            .key
+            .match_indices(Self::PATH_DELIMITER)
+            .nth(levels)
+            .map(|(i, _)| i);
+        let key = dot_pos.and_then(|i| self.key.get(i + 1..))?;
+        Some(Self {
+            key,
+            value: self.value,
+        })
+    }
+
+    fn levels(self) -> impl Iterator<Item = KeyValue<'a>> + 'a {
+        let mut level = 0;
+        let iter = std::iter::from_fn(move || {
+            let val = self.strip(level);
+            level += 1;
+            val
+        });
+        std::iter::once(self).chain(iter)
     }
 }
 
-impl<'a> std::ops::Deref for Node<'a> {
-    type Target = str;
+impl<I> Lexer<I> {
+    const fn new(lines: I) -> Self {
+        Self { lines }
+    }
+}
 
-    fn deref(&self) -> &Self::Target {
-        match self {
-            Self::Key(v) => v,
-            Self::Value(v) => v,
+impl<'de> Lexer<std::str::Lines<'de>> {
+    fn from_str(input: &'de str) -> Self {
+        Self::new(input.lines())
+    }
+}
+
+impl<'de, I> Lexer<I>
+where
+    I: Iterator<Item = &'de str> + Clone,
+{
+    fn children(&self) -> Option<impl Iterator<Item = &'de str>> {
+        fn get_parent(s: &str) -> Option<&str> {
+            s.find(KeyValue::PATH_DELIMITER)
+                .and_then(|i| s.get(..i))
+                .map(|x| x.trim())
         }
+
+        let mut lines = self.lines.clone().peekable();
+        let first = lines.next()?;
+        let parent = get_parent(first);
+        Some(
+            std::iter::once(first)
+                .chain(lines.clone().take_while(move |x| get_parent(x) == parent)),
+        )
     }
 }
 
@@ -85,17 +99,38 @@ impl<'a> std::ops::Deref for Node<'a> {
 mod tests {
     use super::*;
 
-    pub(crate) const INPUT: &'static str = "camera_root.0.interest.trans.x.key.0.data=(0,-0.469822)
-camera_root.0.interest.trans.x.key.0.type=1
-camera_root.0.interest.trans.x.key.1.data=(738,-0.522281,3.31402e-006)
-camera_root.0.interest.trans.x.key.1.type=2
+    #[test]
+    fn lexer_children() {
+        const INPUT: &'static str = "foo.bar
+foo.baz
+foo.quux
+foobar
 ";
 
+        let par = Lexer::from_str(INPUT);
+        let mut lines = INPUT.lines();
+        let mut child = par.children().unwrap();
+        assert_eq!(child.next(), lines.next());
+        assert_eq!(child.next(), lines.next());
+        assert_eq!(child.next(), lines.next());
+        assert_eq!(child.next(), None);
+
+        assert_eq!(
+            Lexer::from_str("").children().and_then(|mut x| x.next()),
+            None
+        );
+    }
+
     #[test]
-    fn new() {
-        let tree = A3daTree::new(INPUT).unwrap();
-        tree.print();
-        println!("----------- WRITE -------------");
-        panic!();
+    fn key_value_new() {
+        assert_eq!(
+            KeyValue::new(" foo.bar = baz "),
+            Some(KeyValue {
+                key: "foo.bar",
+                value: "baz"
+            })
+        );
+        assert_eq!(KeyValue::new("= baz "), None);
+        assert_eq!(KeyValue::new(" bar = "), None);
     }
 }
