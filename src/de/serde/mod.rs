e1dd170beeab575c16544cf835cb91a9e587db45
waelwindows <waelwindows9922@gmail.com>
2022-12-22 21:14:27 +0300

    feat: Complete the implementation of map deser

--- src/de/serde/mod.rs
+++ src/de/serde/mod.rs
@@ -17,11 +17,31 @@ pub fn from_str<'a, T>(s: &'a str) -> Result<T, DeserializerError>
 where
     T: Deserialize<'a>,
 {
-    let mut lex = Lexer::from_str(s);
+    let mut iter = s.lines().peekable();
+    let mut lex = LexerChildren::new(iter);
     T::deserialize(&mut lex)
 }
 
-impl<'de, I: Iterator<Item = &'de str>> Lexer<Peekable<I>> {
+impl<'de, I: Iterator<Item = &'de str>> LexerChildren<'de, I> {
+    fn value(&mut self) -> Result<&'de str, DeserializerError> {
+        let line = self.next();
+        let kv = line
+            .and_then(KeyValue::new)
+            .ok_or(DeserializerError::ExpectedValueNode)?;
+        let val = kv
+            .path()
+            .nth(0)
+            .filter(|x| !x.is_empty())
+            .unwrap_or(kv.value);
+        println!("{:?}, reading {:?}", line, val);
+        Ok(val)
+    }
+    fn parse_atom<T: Deserialize<'de>>(&mut self) -> Result<T, DeserializerError> {
+        atom::from_str(self.value()?).map_err(Into::into)
+    }
+}
+
+impl<'a, 'de, I: Iterator<Item = &'de str>> LexerChildren<'de, Peekable<I>> {
     fn peek_key_value(&mut self) -> Result<KeyValue<'de>, DeserializerError> {
         self.lines
             .peek()
@@ -29,28 +49,11 @@ impl<'de, I: Iterator<Item = &'de str>> Lexer<Peekable<I>> {
             .and_then(KeyValue::new)
             .ok_or(DeserializerError::ExpectedKeyValuePair)
     }
-    fn value(&mut self) -> Result<&'de str, DeserializerError> {
-        dbg!(self.lines.peek());
-        self.peek_key_value().map(|x| x.value).or(self
-            .lines
-            .next()
-            .ok_or(DeserializerError::ExpectedValueNode))
-    }
-    fn convert_value<T>(&mut self) -> Result<T, DeserializerError>
-    where
-        T: FromStr,
-        DeserializerError: From<T::Err>,
-    {
-        self.value()?.parse().map_err(DeserializerError::from)
-    }
-    fn parse_atom<T: Deserialize<'de>>(&mut self) -> Result<T, DeserializerError> {
-        atom::from_str(self.value()?).map_err(Into::into)
-    }
 }
 
-impl<'a, 'de, I: 'de> Deserializer<'de> for &'a mut Lexer<Peekable<I>>
+impl<'a, 'de, I: 'de> Deserializer<'de> for &'a mut LexerChildren<'de, Peekable<I>>
 where
-    I: Iterator<Item = &'de str> + Clone,
+    I: Iterator<Item = &'de str>,
 {
     type Error = DeserializerError;
 
@@ -61,91 +64,91 @@ where
         todo!()
     }
 
-    fn deserialize_bool<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    fn deserialize_bool<V>(mut self, visitor: V) -> Result<V::Value, Self::Error>
     where
         V: Visitor<'de>,
     {
         visitor.visit_bool(self.parse_atom()?)
     }
 
-    fn deserialize_i8<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    fn deserialize_i8<V>(mut self, visitor: V) -> Result<V::Value, Self::Error>
     where
         V: Visitor<'de>,
     {
         visitor.visit_i8(self.parse_atom()?)
     }
 
-    fn deserialize_i16<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    fn deserialize_i16<V>(mut self, visitor: V) -> Result<V::Value, Self::Error>
     where
         V: Visitor<'de>,
     {
         visitor.visit_i16(self.parse_atom()?)
     }
 
-    fn deserialize_i32<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    fn deserialize_i32<V>(mut self, visitor: V) -> Result<V::Value, Self::Error>
     where
         V: Visitor<'de>,
     {
         visitor.visit_i32(self.parse_atom()?)
     }
 
-    fn deserialize_i64<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    fn deserialize_i64<V>(mut self, visitor: V) -> Result<V::Value, Self::Error>
     where
         V: Visitor<'de>,
     {
         visitor.visit_i64(self.parse_atom()?)
     }
 
-    fn deserialize_u8<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    fn deserialize_u8<V>(mut self, visitor: V) -> Result<V::Value, Self::Error>
     where
         V: Visitor<'de>,
     {
         visitor.visit_u8(self.parse_atom()?)
     }
 
-    fn deserialize_u16<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    fn deserialize_u16<V>(mut self, visitor: V) -> Result<V::Value, Self::Error>
     where
         V: Visitor<'de>,
     {
         visitor.visit_u16(self.parse_atom()?)
     }
 
-    fn deserialize_u32<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    fn deserialize_u32<V>(mut self, visitor: V) -> Result<V::Value, Self::Error>
     where
         V: Visitor<'de>,
     {
         visitor.visit_u32(self.parse_atom()?)
     }
 
-    fn deserialize_u64<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    fn deserialize_u64<V>(mut self, visitor: V) -> Result<V::Value, Self::Error>
     where
         V: Visitor<'de>,
     {
         visitor.visit_u64(self.parse_atom()?)
     }
 
-    fn deserialize_f32<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    fn deserialize_f32<V>(mut self, visitor: V) -> Result<V::Value, Self::Error>
     where
         V: Visitor<'de>,
     {
         visitor.visit_f32(self.parse_atom()?)
     }
 
-    fn deserialize_f64<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    fn deserialize_f64<V>(mut self, visitor: V) -> Result<V::Value, Self::Error>
     where
         V: Visitor<'de>,
     {
         visitor.visit_f64(self.parse_atom()?)
     }
 
-    fn deserialize_char<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    fn deserialize_char<V>(mut self, visitor: V) -> Result<V::Value, Self::Error>
     where
         V: Visitor<'de>,
     {
         todo!()
     }
 
-    fn deserialize_str<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    fn deserialize_str<V>(mut self, visitor: V) -> Result<V::Value, Self::Error>
     where
         V: Visitor<'de>,
     {
@@ -239,59 +242,7 @@ where
     where
         V: Visitor<'de>,
     {
-        #[derive(Clone)]
-        struct MapParser<'a, T> {
-            iter: LexerChildren<'a, T>,
-            // cur: KeyValue<'a>,
-        }
-
-        impl<'a, T: Iterator<Item = &'a str> + Clone + 'a> MapParser<'a, T> {
-            fn peek_key_value(&mut self) -> Option<KeyValue<'a>> {
-                let mut iter = self.iter.clone().peekable();
-                iter.peek().cloned().and_then(KeyValue::new)
-            }
-        }
-
-        impl<'a, T: Iterator<Item = &'a str> + Clone + 'a> MapAccess<'a> for MapParser<'a, T> {
-            type Error = DeserializerError;
-
-            fn next_key_seed<K>(&mut self, seed: K) -> Result<Option<K::Value>, Self::Error>
-            where
-                K: DeserializeSeed<'a>,
-            {
-                println!("reading map key");
-                let keyval = if let Some(keyval) = self.peek_key_value() {
-                    keyval
-                } else {
-                    return Ok(None);
-                };
-                let ident = keyval.path().next().expect("path");
-                dbg!(ident);
-                let lexer = AtomParser(ident);
-                seed.deserialize(lexer).map(Some).map_err(Into::into)
-            }
-
-            fn next_value_seed<V>(&mut self, seed: V) -> Result<V::Value, Self::Error>
-            where
-                V: DeserializeSeed<'a>,
-            {
-                println!("reading map value");
-                let mut iter = LexerChildren::new(self.clone().iter)
-                    .strip_prefix(true)
-                    .chain(std::iter::once(
-                        self.peek_key_value().expect("key value").value,
-                    ))
-                    .peekable();
-                dbg!(iter.peek());
-                let mut lexer = Lexer::new(iter);
-                self.iter.next();
-                seed.deserialize(&mut lexer)
-            }
-        }
-
-        let mut iter = self.lines.clone().peekable();
-        let children = LexerChildren::new(iter);
-        visitor.visit_map(MapParser { iter: children })
+        visitor.visit_map(self)
     }
 
     fn deserialize_struct<V>(
@@ -303,7 +254,7 @@ where
     where
         V: Visitor<'de>,
     {
-        todo!()
+        self.deserialize_map(visitor)
     }
 
     fn deserialize_enum<V>(
@@ -318,7 +269,7 @@ where
         todo!()
     }
 
-    fn deserialize_identifier<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    fn deserialize_identifier<V>(mut self, visitor: V) -> Result<V::Value, Self::Error>
     where
         V: Visitor<'de>,
     {
@@ -335,6 +286,39 @@ where
         V: Visitor<'de>,
     {
         self.deserialize_unit(visitor)
+    }
+}
+
+impl<'de, I: Iterator<Item = &'de str> + 'de> MapAccess<'de> for LexerChildren<'de, Peekable<I>> {
+    type Error = DeserializerError;
+
+    fn next_key_seed<K>(&mut self, seed: K) -> Result<Option<K::Value>, Self::Error>
+    where
+        K: DeserializeSeed<'de>,
+    {
+        println!("reading map key");
+        // dbg!(self.peek());
+        // dbg!(self.cache, self.prefix, self.lines.peek());
+        if self.is_finished() {
+            println!("------------- done ----------");
+            return Ok(None);
+        }
+        // dbg!(self.lines.peek());
+        let val = seed.deserialize(&mut *self).map(Some);
+        self.increment_prefix_level();
+        val
+    }
+
+    fn next_value_seed<V>(&mut self, seed: V) -> Result<V::Value, Self::Error>
+    where
+        V: DeserializeSeed<'de>,
+    {
+        println!("reading map value");
+        // dbg!(self.next());
+        let val = seed.deserialize(&mut *self);
+        self.decrement_prefix_level();
+        self.next();
+        val
     }
 }
 
@@ -359,7 +343,7 @@ quux = 4
         assert_eq!(from_str(input), Ok(expected));
     }
     #[test]
-    fn read_map_nested() {
+    fn read_nested_map() {
         let input = "foo.bar = 1
 foo.baz = 2
 bar.baz = 3
