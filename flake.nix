8afd3bfa068935071fd8876c33ef74351b1f4fae
waelwindows <waelwindows9922@gmail.com>
2025-02-28 18:30:54 +0300

    build(nix): Use latest stable

--- flake.nix
+++ flake.nix
@@ -17,39 +17,40 @@
     flake-utils,
     rust-overlay,
     crane,
-  }: let
-  in
+  }:
     flake-utils.lib.eachDefaultSystem
-    (system: let
+    (
+      system: let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ (import rust-overlay) ];
+          overlays = [(import rust-overlay)];
         };
         craneLib = crane.lib.${system};
-      in rec {
+      in {
         packages = rec {
           serde_divatree = craneLib.buildPackage {
-              src = craneLib.cleanCargoSource ./.;
-              doCheck = false;
+            src = craneLib.cleanCargoSource ./.;
+            doCheck = false;
 
-              # Add extra inputs here or any other derivation settings
-              # doCheck = true;
-              # buildInputs = [];
-              # nativeBuildInputs = [];
-            };
+            # Add extra inputs here or any other derivation settings
+            # doCheck = true;
+            # buildInputs = [];
+            # nativeBuildInputs = [];
+          };
           default = serde_divatree;
         };
-        devShells.default = with pkgs; pkgs.mkShell rec {
-          nativeBuildInputs = [
-            pkg-config
-          ];
-          buildInputs = [
-            (rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
-              extensions = [ "rust-src" "rust-analyzer" ];
-              targets = [];
-            }))
-          ];
-        };
+        devShells.default = with pkgs;
+          pkgs.mkShell rec {
+            nativeBuildInputs = [
+              pkg-config
+            ];
+            buildInputs = [
+              (rust-bin.stable.latest.default.override {
+                extensions = ["rust-src" "rust-analyzer"];
+                targets = [];
+              })
+            ];
+          };
       }
     );
 }
